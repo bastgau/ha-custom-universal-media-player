5924f89fc0ac034b25293f20baeba42ca371305b
Bastien Gautier <contact@bastiengautier.com>
2026-02-20 01:14:24 +0000

    refactor: move constants in const.py

--- custom_components/custom_universal_media_player/const.py
+++ custom_components/custom_universal_media_player/const.py
@@ -1,3 +1,9 @@
 """Provides the constants needed for component."""
 
+ATTR_ACTIVE_CHILD = "active_child"
 ATTR_ENTITY_PICTURE_LOCAL = "entity_picture_local"
+CONF_ACTIVE_CHILD_TEMPLATE = "active_child_template"
+CONF_ATTRS = "attributes"
+CONF_BROWSE_MEDIA_ENTITY = "browse_media_entity"
+CONF_CHILDREN = "children"
+CONF_COMMANDS = "commands"
